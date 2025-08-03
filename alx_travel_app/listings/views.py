997de313af204c1372e1f9647f03fe7c22d7e1f0
de-sam <samsoncoded@gmail.com>
2025-08-03 19:39:10 +0100

    Enhaced mobile view

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# listings/views.py
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class ListingViewSet(viewsets.ModelViewSet):
+    """
+    API viewset for CRUD operations on Listings
+    """
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+    permission_classes = [IsAuthenticated]
+
+class BookingViewSet(viewsets.ModelViewSet):
+    """
+    API viewset for CRUD operations on Bookings
+    """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
